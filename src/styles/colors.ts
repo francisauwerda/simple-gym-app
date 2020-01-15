a7e140168e853ae2f1493326d86ac69d94c5959a
Francis Auwerda <fauwerda@gmail.com>
2020-01-15 08:00:04 +0100

    Changing colour opacity

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,6 +1,10 @@
-const blue = '#2196F3';
-const yellow = '#FFEB3B';
-const red = '#F44336';
+const colourOpacity = '0.6';
+// const blue = '#2196F3';
+const blue = `rgba(33, 150, 243, ${colourOpacity})`;
+// const yellow = '#FFEB3B';
+const yellow = `rgba(255, 235, 59, ${colourOpacity})`;
+// const red = '#F44336';
+const red = `rgba(244, 67, 54, ${colourOpacity})`;
 
 export default {
   easy: blue,
