01dc4c4341b76b22cf3de4d9f1746b3ddc61c3e3
Francis Auwerda <fauwerda@gmail.com>
2020-10-25 21:25:15 +0100

    Adjust styling of sort component

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -17,39 +17,44 @@ const Sort = ({
   const { direction, sorting } = settings;
 
   return (
-    <TouchableComponent
-      onPress={() => {
-        setSettings({
-          ...settings,
-          direction: direction === Direction.ASC ? Direction.DESC : Direction.ASC,
-        });
-      }}
-      onLongPress={() => {
-        setSettings({
-          ...settings,
-          sorting: sorting === Sorting.name ? Sorting.lastModified : Sorting.name,
-        });
-      }}
-    >
-      <View style={styles.sortContainer}>
-        <Text style={styles.sortText}>
-          {direction === Direction.ASC ? '☝️' : '👇'}
-        </Text>
-        <Text style={styles.sortText}>
-          {sorting === Sorting.name ? 'Name' : 'Recent'}
-        </Text>
-      </View>
-    </TouchableComponent>
+    <View style={styles.outerContainer}>
+      <TouchableComponent
+        onPress={() => {
+          setSettings({
+            ...settings,
+            direction: direction === Direction.ASC ? Direction.DESC : Direction.ASC,
+          });
+        }}
+        onLongPress={() => {
+          setSettings({
+            ...settings,
+            sorting: sorting === Sorting.name ? Sorting.lastModified : Sorting.name,
+          });
+        }}
+      >
+        <View style={styles.sortContainer}>
+          <Text style={styles.sortText}>
+            {direction === Direction.ASC ? '☝️' : '👇'}
+          </Text>
+          <Text style={styles.sortText}>
+            {sorting === Sorting.name ? 'Name' : 'Recent'}
+          </Text>
+        </View>
+      </TouchableComponent>
+    </View>
   );
 };
 
 export default Sort;
 
 const styles = StyleSheet.create({
+  outerContainer: {
+    display: 'flex',
+    alignItems: 'flex-end',
+  },
   sortContainer: {
     flexDirection: 'row',
-    paddingVertical: 15,
-    paddingRight: 5,
+    padding: 15,
     justifyContent: 'flex-end',
     alignItems: 'center',
     marginTop: 10,
